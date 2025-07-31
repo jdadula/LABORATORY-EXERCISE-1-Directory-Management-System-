53cb5dc589837b1144de6374969113b9be7590c2
jdadula <jonneldadula09@gmail.com>
2025-07-31 11:52:57 +0800

    Add files via upload

---
+++ DisplayMenu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <direct.h>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    cout << "\nDirectory Management System\n";
+    cout << "[1] List Files\n";
+    cout << "[2] Create Directory\n";
+    cout << "[3] Change Directory\n";
+    cout << "[4] Exit\n";
+    cout << "Enter your choice: ";
+}
